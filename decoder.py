c784a6760a94f255d095ad251e9ec4bc0c8d0759
Nicholas Liu <nicholasliu2015@gmail.com>
2021-04-22 09:40:58 -0700

    all files

---
+++ decoder.py
@@ -0,0 +1,36 @@
+import string
+
+def decode(encodedMsg, secretChar):
+
+    charsInMsg = 0
+    decodedMsg = ""
+
+    for char in encodedMsg:
+        if char == secretChar:
+            charsInMsg += 1
+
+    if charsInMsg == 0:
+        return "NA"
+
+    fakeLetters = len(encodedMsg) / charsInMsg
+
+    for i in range(1, charsInMsg + 1):
+        decodedMsg += encodedMsg[fakeLetters * i - 1]
+
+    return decodedMsg
+
+def main():
+
+    encodedMsg = ""
+    efile = open("msg.txt", 'r')
+    for line in efile:
+        encodedMsg += line
+    efile.close()
+
+    randCharList = list(string.ascii_lowercase) + (list(string.ascii_uppercase))
+    for i in range(52):
+        secretChar = randCharList[i]
+        decodedMsg = decode(encodedMsg, secretChar)
+        print(randCharList[i] + " : " + decodedMsg)
+
+main()
---
+++ encoder.py
@@ -0,0 +1,44 @@
+import random
+import string
+
+def encode(msg):
+    randCharList = list(string.ascii_lowercase) + (list(string.ascii_uppercase))
+    #amount of fake letters changes every time
+    encodeLength = random.randrange(50,70)
+    #letter changes evry time
+    encodeChar = random.choice(randCharList)
+    encodedMsg = ""
+    for i in range(len(msg)):
+        fakeLetterCount = 0
+        #different spot for encodedChar each time
+        encodeCharSpot = random.randrange(1, encodeLength - 1)
+
+        while fakeLetterCount < encodeLength:
+            if fakeLetterCount == encodeCharSpot and encodeChar != msg[i]:
+                encodedMsg += encodeChar
+
+            else:
+                fakeLetter = random.choice(randCharList)
+                while fakeLetter == encodeChar:
+                    fakeLetter = random.choice(randCharList)
+                encodedMsg += fakeLetter
+            fakeLetterCount += 1
+
+        encodedMsg += msg[i]
+
+    return encodedMsg, encodeChar
+
+def main():
+
+    msg = ""
+    efile = open("msg.txt", 'r')
+    for line in efile:
+        msg += line
+    efile.close()
+
+    encodedMsg, secretChar = encode(msg)
+
+    print("\n%s" % encodedMsg)
+    print("Secret character: %s\n" % secretChar)
+
+main()
